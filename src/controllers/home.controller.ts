7a70bd4ef7f969f6b6dd7b010987248acd054975
c0d3x <xzaeitmurft@gmail.com>
2025-07-04 12:07:32 -0500

    cambios a revisar

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,5 +1,21 @@
-import type { Request, Response } from 'express';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
 
-export const homeController = (_req: Request, res: Response) => {
-  res.render('pages/index', { title: 'Bienvenido a Anthsoft 🚀' });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const homeController = async (req: express.Request, res: express.Response) => {
+
+  try {
+    const jsonPath = path.join(__dirname, '../data/servicios.json');
+    const file = await readFile(jsonPath, 'utf-8');
+    const servicios = JSON.parse(file);
+
+    res.render('pages/home', { servicios });
+  } catch (error) {
+    console.error('Error cargando servicios:', error);
+    res.status(500).send('Error al cargar los servicios.');
+  }
 };
